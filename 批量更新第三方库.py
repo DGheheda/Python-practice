80e5389754f6087c64f1a736a8248cd7735eb8f1
e <642755426@qq.com>
2019-06-22 09:40:45 +0800

    testing

--- 批量更新第三方库.py
+++ 批量更新第三方库.py
@@ -8,7 +8,8 @@ def xvc():
 
     # 读取指令而不是返回值是要用os.popen().read()，否则只会返回返回值
     f = json.loads(os.popen("pip list -o --format json").read())
-    if len(f) == 0:  # 确定是不是已经全部更新完毕
+    a=len(f)
+    if a == 0:  # 确定是不是已经全部更新完毕
         print("all module is updated")
     else:
         for i in f:
