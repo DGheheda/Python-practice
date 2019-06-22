57c2c5ccaec9ce19102b8d777e5008810360c871
e <642755426@qq.com>
2019-06-22 09:38:22 +0800

    testing

--- 批量更新第三方库.py
+++ 批量更新第三方库.py
@@ -1,13 +1,19 @@
 import json
 import os
 import pysnooper
-@pysnooper()
-f = json.loads(os.popen("pip list -o --format json").read())# 读取指令而不是返回值是要用os.popen().read()，否则只会返回返回值
-if len(f) == 0:  # 确定是不是已经全部更新完毕
-    print("all module is updated")
-else:
-    for i in f:
-        myky='name'
-        for u in i.keys():
-            if u == myky:
-                print(os.popen("pip install --upgrade "+i[u]).read())
+
+@pysnooper.snoop()
+def xvc():
+
+
+    # 读取指令而不是返回值是要用os.popen().read()，否则只会返回返回值
+    f = json.loads(os.popen("pip list -o --format json").read())
+    if len(f) == 0:  # 确定是不是已经全部更新完毕
+        print("all module is updated")
+    else:
+        for i in f:
+            myky = 'name'
+            for u in i.keys():
+                if u == myky:
+                    print(os.popen("pip install --upgrade "+i[u]).read())
+xvc()
