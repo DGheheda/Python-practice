f7baf4d4dbba18a9989f88351194ded79d840d3a
DGheheda <642755426@qq.com>
2021-07-26 21:55:37 +0800

    liangliang

--- tabulatttt.py
+++ tabulatttt.py
@@ -1,11 +1,16 @@
 import pdfplumber
 import xlwt
-pdf=pdfplumber.open(r"618.pdf")
-first_page= pdf.pages[2]
-title=first_page.extract_text()#获取报告标题所属字符串
-#标题索引值title[17:44]
-second_page=pdf.pages[6]
-table1=second_page.extract_tables()
-wbk = xlwt.Workbook()
-sheet = wbk.add_sheet(title[17:44],cell_overwrite_ok=True)#修改工作簿名称
-wbk.save("table.xls")
+def convert(path):
+    pdf=pdfplumber.open(path)
+    first_page= pdf.pages[2]
+    title=first_page.extract_text()#获取报告标题所属字符串
+    #标题索引值title[17:44]
+    second_page=pdf.pages[6]
+    table1=second_page.extract_tables()
+    wbk = xlwt.Workbook()
+    sheet = wbk.add_sheet(title[17:44],cell_overwrite_ok=True)#修改工作簿名称
+    for i in range(len(table1[0][0])):
+        sheet.write(0,i,table1[0][0][i])
+        wbk.save('618.xls')
+    return
+convert('618.pdf')
