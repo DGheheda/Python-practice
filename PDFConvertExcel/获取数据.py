a6bcd712a98c0b2c67eb67ba2268f71d54ca213f
DGheheda <642755426@qq.com>
2021-07-27 22:08:24 +0800

    PDFConvertExcel

--- PDFConvertExcel/获取数据.py
+++ PDFConvertExcel/获取数据.py
@@ -2,14 +2,14 @@ from os import path
 import pdfplumber
 path=r'PDFConvertExcel\20210723_100613_CloudPoint(报告 1).pdf'
 def titleget():
+    global pdf#从函数内参数改为全局函数，减少程序体积
     pdf=pdfplumber.open(path)#打开PDF文件获取数据
     xltitle=pdf.pages[2].extract_text()[18:44]#截取数据标题
-    return xltitle
+    return xltitle,path
 def data1titleget():
-    pdf=pdfplumber.open(path)
     data1titlepage=pdf.pages[6].extract_tables()[0][0]
     return data1titlepage
 def data2titleget():
-    pdf=pdfplumber.open(path)
     data2titlepage=pdf.pages[8].extract_tables()[0][0]
     return data2titlepage
+print(titleget())
