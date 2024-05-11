1b7ebd07c5d767aec8d80adb0898225f19854db8
DGheheda <642755426@qq.com>
2024-05-11 22:07:03 +0800

    fuuuuuuuuck~

--- result check/practice.py
+++ result check/practice.py
@@ -1,12 +1,11 @@
 import csv
 import codecs
-# source_file=r'C:\Users\David\Desktop\Python-practice\result check\LI\20240506185821.DFQ'
-# target_encoding='GBK'
-# with open(source_file,'r',encoding='cp936') as zhfile:
-#     content=zhfile.read()
-# with codecs.open(r'1.txt','w',target_encoding) as f:
-#     f.write(content)
-with codecs.open(r'C:\Users\David\Desktop\Python-practice\result check\LI\20240506185821.DFQ','r','cp936') as cf:
-    cfreader=csv.reader(cf,dialect='excel',delimiter=' ',)
+from re import split
+with codecs.open(r'C:\Users\David\Desktop\Python-practice\result check\LI\20240506185821.DFQ','r',encoding='MacRoman') as cf:
+    cfreader=csv.reader(cf,dialect='excel',delimiter=':',)
+    dict1={}
     for i in cfreader:
-        print(i)
+       if len(i)>0:
+            a=i[0]
+            print(a.split())
+        
