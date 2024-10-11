24c4694f02186a72f77bbeb7da13f2d9217d7bc1
DGheheda <642755426@qq.com>
2024-10-11 14:18:53 +0800

    fast finish~

--- result check/practice.py
+++ result check/practice.py
@@ -1,8 +1,1 @@
-import csv
-import codecs
-source_file=r'C:\Users\David\Desktop\Python-practice\result check\LI\20240506185821.DFQ'
-target_encoding='GBK'
-with open(source_file,'r',encoding='cp936') as zhfile:
-    content=zhfile.read()
-with codecs.open(r'1.txt','w',target_encoding) as f:
-    f.write(content)
+import chardet
